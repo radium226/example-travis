996bf72488fec87013ea27b6488c6aa6d345fc40
radium226 <adrien.besnard@gmail.com>
2014-05-27 08:13:55 +0200

    Modify Travis Config

---
+++ .travis-tools/javadoc.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [ "$TRAVIS_REPO_SLUG" == "radium226/example-travis" ] && [ "$TRAVIS_JDK_VERSION" == "oraclejdk7" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
+
+echo -e "Publishing javadoc...\n"
+
+  cp -R build/apidocs $HOME/javadoc
+
+  cd $HOME
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "travis-ci"
+  git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/radium226/example-travis gh-pages
+
+  cd gh-pages
+  git rm -rf ./javadoc
+  cp -Rf $HOME/javadoc ./javadoc
+  git add -f .
+  git commit -m "Lastest javadoc on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
+  git push -fq origin gh-pages
+
+  echo -e "Published Javadoc to gh-pages.\n"
+  
+fi
+
